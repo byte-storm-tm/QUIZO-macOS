d3f97cc3e05affa0865fe6ae8a775fd5a563b03e
Char <91511432+Charmander58GitHub@users.noreply.github.com>
2025-03-06 18:17:54 -0500

    Add files via upload

---
+++ HelloWorld.swift
@@ -0,0 +1,22 @@
+let hello = "Hello, world!"
+
+// ¿Qué haces mirando esto? Te pasaste de curioso 👀
+// El código es cerrado y no se publicará
+let textArtHola = """
+ #     #  #######  #        #    
+ #     #  #     #  #       # #   
+ #     #  #     #  #      #   #  
+ #######  #     #  #     #     # 
+ #     #  #     #  #     ####### 
+ #     #  #     #  #     #     # 
+ #     #  #######  ##### #     # 
+                                 
+ ¡Bienvenido a QUIZO macOS! 
+ 
+ Creado originalmente por ByteStorm & Katsito
+ Versión macOS desarrollada y optimizada por Charmander58
+ © 2024-2025 ByteStorm - Todos los derechos reservados
+ © 2021-2025 Chardev's team - Todos los derechos reservados
+"""
+
+print(textArtHola)
